02dc72ee28a9a40c995c933a267c087da1ada3f3
Ulisses <uli.felix8484@gmail.com>
2025-08-18 01:25:20 -0300

    Update Salesman.java

--- src/domain/Salesman.java
+++ src/domain/Salesman.java
@@ -1,16 +1,37 @@
+package domain;
+
 public non-sealed class Salesman extends Employee {
     private double percentPerSold;
+    private double soldAmount;
+
+    public double getSoldAmount() {
+        return soldAmount;
+    }
+
+    public void setSoldAmount(double soldAmount) {
+        this.soldAmount = soldAmount;
+    }
 
     public Salesman() {
     }
 
-    public Salesman(String code, String name, String address, int age, double salary, double percentPerSold) {
+    public Salesman(String code, String name, String address, int age, double salary, double percentPerSold, double soldAmount) {
         super(code, name, address, age, salary);
         this.percentPerSold = percentPerSold;
+        this.soldAmount = soldAmount;
     }
     public String getCode() {
         return "SL" + super.getCode();
     }
+    public double getFullSalary(double extra) {
+        return this.getFullSalary() + extra;
+    }
+
+    @Override
+    public double getFullSalary() {
+        return salary += (this.percentPerSold + this.soldAmount)/100;
+    }
+
     public double getPercentPerSold() {
         return percentPerSold;
     }
