4049c3672b8b3dde14ef4b9cc68da925d6adefad
uliazu2 <uli.felix8484@gmail.com>
2025-08-21 15:56:51 -0300

    listas

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -1,12 +1,15 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+
+import static java.lang.Math.random;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args){
-        List<String> test = new ArrayList<String>();
-        test.add("a");
-        var a = test.get(0);
+        int[] codes = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        System.out.println(Arrays.toString(codes));
+        List<Integer> codes2 = new ArrayList<>();
     }
 }
